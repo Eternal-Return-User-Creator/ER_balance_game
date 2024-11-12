0b0e34d059aa66f17e8e5f7765e29148f8fc7fcc
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-12 09:32:56 +0900

    chore(question): 누락된 주석 추가

--- src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
+++ src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
@@ -23,6 +23,11 @@ public class QuestionRepository implements IQuestionRepository {
         this.entityManager = entityManager;
     }
 
+    /**
+     * 질문을 저장한다.
+     * @param question 저장할 질문
+     * @return 저장된 질문
+     */
     @Override
     @Transactional
     public Question save(Question question) {
@@ -30,12 +35,22 @@ public class QuestionRepository implements IQuestionRepository {
         return question;
     }
 
+    /**
+     * id로 질문을 조회한다.
+     * @param id 질문 id
+     * @return id로 조회된 질문
+     */
     @Override
     public Optional<Question> findById(Long id) {
         Question question = entityManager.find(Question.class, id);
         return Optional.ofNullable(question);
     }
 
+    /**
+     * 질문을 포함하는 질문을 조회한다. 즉, 질문A 또는 질문B가 question인 질문을 조회한다.
+     * @param question 질문
+     * @return 질문을 포함하는 질문
+     */
     @Override
     public List<Question> findByQuestion(String question) {
         String jpql = "SELECT q FROM Question q WHERE q.questionA = :question OR q.questionB = :question";
@@ -44,6 +59,14 @@ public class QuestionRepository implements IQuestionRepository {
                 .getResultList();
     }
 
+    /**
+     * 두 질문을 동시에 만족하는 질문을 조회한다.
+     * 즉, 질문A가 questionA이고 질문B가 questionB 이거나, 질문A가 questionB이고 질문B가 questionA인 질문을 조회한다.
+     * @apiNote 두 질문이 동시에 만족하는 질문이 없을 경우 Optional.empty()를 반환한다.
+     * @param questionA 질문A
+     * @param questionB 질문B
+     * @return 두 질문을 동시에 만족하는 질문
+     */
     @Override
     public Optional<Question> findByQuestionAB(String questionA, String questionB) {
         String jpql = "SELECT q FROM Question q WHERE (q.questionA = :questionA AND q.questionB = :questionB) OR (q.questionA = :questionB AND q.questionB = :questionA)";
@@ -77,6 +100,9 @@ public class QuestionRepository implements IQuestionRepository {
         return Optional.empty();
     }
 
+    /**
+     * 모든 질문을 삭제한다.
+     */
     @Override
     @Transactional
     public void deleteAll() {
