c41f563476211fd12e89d38473df55e5951a862d
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-12 09:53:08 +0900

    test(question): 랜덤하게 조회하는 비즈니스 로직을 서비스로 이동

--- src/test/java/nyj001012/er_bal/repository/QuestionRepositoryTest.java
+++ src/test/java/nyj001012/er_bal/repository/QuestionRepositoryTest.java
@@ -46,29 +46,4 @@ public class QuestionRepositoryTest {
         assertThat(savedQuestion.getQuestionB()).isEqualTo("질문2");
     }
 
-    @Test
-    public void 질문_조회_테스트() {
-        // 저장된 질문이 3개일 때
-        for (int i = 0; i < 3; i++) {
-            Date dateTime = new Date();
-            Question question = new Question();
-            question.setQuestionA("질문 A" + i);
-            question.setQuestionB("질문 B" + (i + 1));
-            question.setCreatedDate(dateTime);
-            question.setUpdatedDate(dateTime);
-            question.setAChoiceCount(0L);
-            question.setBChoiceCount(0L);
-            questionRepository.save(question);
-        }
-
-        // 3개의 랜덤한 질문이 조회되고
-        Set<Long> selectedQuestionIds = new HashSet<>();
-        for (int i = 0; i < 3; i++) {
-            Optional<Question> question = questionRepository.findOneRandom();
-            assertThat(question).isPresent();
-            assertThat(selectedQuestionIds).doesNotContain(question.get().getId());
-        }
-        // 더 이상 조회되는 질문이 없어야 한다.
-        assertThat(questionRepository.findOneRandom()).isEmpty();
-    }
 }
--- src/test/java/nyj001012/er_bal/service/QuestionServiceTest.java
+++ src/test/java/nyj001012/er_bal/service/QuestionServiceTest.java
@@ -9,7 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.event.annotation.AfterTestClass;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -151,5 +153,31 @@ public class QuestionServiceTest {
             assertThat(q.getQuestionA()).isEqualTo(question.getQuestionA());
             assertThat(q.getQuestionB()).isEqualTo(question.getQuestionB());
         }, Assertions::fail);
+    }
+
+    @Test
+    public void 질문을_무작위로_조회() {
+        // 저장된 질문이 3개일 때
+        for (int i = 0; i < 3; i++) {
+            Date dateTime = new Date();
+            Question question = new Question();
+            question.setQuestionA("질문 A" + i);
+            question.setQuestionB("질문 B" + (i + 1));
+            question.setCreatedDate(dateTime);
+            question.setUpdatedDate(dateTime);
+            question.setAChoiceCount(0L);
+            question.setBChoiceCount(0L);
+            questionRepository.save(question);
+        }
+
+        // 3개의 랜덤한 질문이 조회되고
+        Set<Long> selectedQuestionIds = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            Optional<Question> question = questionService.getRandom();
+            assertThat(question).isPresent();
+            assertThat(selectedQuestionIds).doesNotContain(question.get().getId());
+        }
+        // 더 이상 조회되는 질문이 없어야 한다.
+        assertThat(questionService.getRandom()).isEmpty();
     }
 }
