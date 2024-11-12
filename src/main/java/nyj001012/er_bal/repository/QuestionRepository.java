14556c6290e47d4a45615cfbbfcd9dce689d8fbf
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-12 09:50:11 +0900

    feat(question): 랜덤하게 조회하는 비즈니스 로직을 서비스로 이동

--- src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
+++ src/main/java/nyj001012/er_bal/repository/QuestionRepository.java
@@ -6,10 +6,8 @@ import jakarta.transaction.Transactional;
 import nyj001012.er_bal.domain.Question;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Repository
 public class QuestionRepository implements IQuestionRepository {
@@ -17,7 +15,6 @@ public class QuestionRepository implements IQuestionRepository {
     @PersistenceContext
     private final EntityManager entityManager;
 
-    private final Set<Long> selectedQuestionIds = new HashSet<>();
 
     public QuestionRepository(EntityManager entityManager) {
         this.entityManager = entityManager;
@@ -25,6 +22,7 @@ public class QuestionRepository implements IQuestionRepository {
 
     /**
      * 질문을 저장한다.
+     *
      * @param question 저장할 질문
      * @return 저장된 질문
      */
@@ -37,6 +35,7 @@ public class QuestionRepository implements IQuestionRepository {
 
     /**
      * id로 질문을 조회한다.
+     *
      * @param id 질문 id
      * @return id로 조회된 질문
      */
@@ -48,6 +47,7 @@ public class QuestionRepository implements IQuestionRepository {
 
     /**
      * 질문을 포함하는 질문을 조회한다. 즉, 질문A 또는 질문B가 question인 질문을 조회한다.
+     *
      * @param question 질문
      * @return 질문을 포함하는 질문
      */
@@ -62,10 +62,11 @@ public class QuestionRepository implements IQuestionRepository {
     /**
      * 두 질문을 동시에 만족하는 질문을 조회한다.
      * 즉, 질문A가 questionA이고 질문B가 questionB 이거나, 질문A가 questionB이고 질문B가 questionA인 질문을 조회한다.
-     * @apiNote 두 질문이 동시에 만족하는 질문이 없을 경우 Optional.empty()를 반환한다.
+     *
      * @param questionA 질문A
      * @param questionB 질문B
      * @return 두 질문을 동시에 만족하는 질문
+     * @apiNote 두 질문이 동시에 만족하는 질문이 없을 경우 Optional.empty()를 반환한다.
      */
     @Override
     public Optional<Question> findByQuestionAB(String questionA, String questionB) {
@@ -79,25 +80,28 @@ public class QuestionRepository implements IQuestionRepository {
 
     /**
      * 랜덤으로 하나의 질문을 조회한다.
-     * @apiNote 중복 조회되지 않도록 selectedQuestionIds에 조회된 질문의 id를 저장한다.
+     *
      * @return 랜덤으로 조회된 질문
+     * @apiNote 중복 조회되지 않도록 selectedQuestionIds에 조회된 질문의 id를 저장한다.
      */
     @Override
-    public Optional<Question> findOneRandom() {
-        int totalSize = entityManager.createQuery("SELECT COUNT(q) FROM Question q", Long.class)
+    public Question findOneRandom() {
+        String jpql = "SELECT q FROM Question q ORDER BY FUNCTION('RAND')";
+        return entityManager.createQuery(jpql, Question.class)
+                .setMaxResults(1)
+                .getSingleResult();
+    }
+
+    /**
+     * 질문의 총 개수를 조회한다.
+     *
+     * @return 질문의 총 개수
+     */
+    @Override
+    public int count() {
+        return entityManager.createQuery("SELECT COUNT(q) FROM Question q", Long.class)
                 .getSingleResult()
                 .intValue();
-        while (selectedQuestionIds.size() < totalSize) {
-            String jpql = "SELECT q FROM Question q ORDER BY FUNCTION('RAND')";
-            Question question = entityManager.createQuery(jpql, Question.class)
-                    .setMaxResults(1)
-                    .getSingleResult();
-            if (!selectedQuestionIds.contains(question.getId())) {
-                selectedQuestionIds.add(question.getId());
-                return Optional.of(question);
-            }
-        }
-        return Optional.empty();
     }
 
     /**
