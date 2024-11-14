06368f3bc00eeefb63fe788beb5950e78863a60c
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-14 19:39:06 +0900

    test(question): 비속어가 포함된 질문을 등록할 때의 테스트 케이스 추가

--- src/test/java/nyj001012/er_bal/controller/QuestionControllerTest.java
+++ src/test/java/nyj001012/er_bal/controller/QuestionControllerTest.java
@@ -114,7 +114,46 @@ public class QuestionControllerTest {
                     });
             verify(questionService, times(1)).post(any(Question.class));
         }
-    }
+
+        @Test
+        public void 질문_등록_실패_질문에_비속어가_포함된_경우() throws Exception {
+            String questionJson = """
+                    {
+                      "questionA": "ㅆㅂ이라고 욕한다.",
+                      "questionB": "욕설이 포함된 질문입니다."
+                    }""";
+            given(questionService.post(any(Question.class)))
+                    .willThrow(new IllegalArgumentException("욕설은 사용할 수 없습니다."));
+            mockMvc.perform(post("/api/question")
+                            .contentType("application/json")
+                            .content(questionJson))
+                    .andExpect(result -> {
+                        assertThat(result.getResponse().getStatus()).isEqualTo(400);
+                        assertThat(result.getResponse().getContentAsString())
+                                .isEqualTo("욕설은 사용할 수 없습니다.");
+                    });
+            verify(questionService, times(1)).post(any(Question.class));
+        }
+
+        @Test
+        public void 질문_등록_실패_질문이_중복된_경우() throws Exception {
+            String questionJson = """
+                    {
+                      "questionA": "질문A 입니다.",
+                      "questionB": "질문B 입니다."
+                    }""";
+            given(questionService.post(any(Question.class)))
+                    .willThrow(new IllegalArgumentException("이미 등록된 질문입니다."));
+            mockMvc.perform(post("/api/question")
+                            .contentType("application/json")
+                            .content(questionJson))
+                    .andExpect(result -> {
+                        assertThat(result.getResponse().getStatus()).isEqualTo(400);
+                        assertThat(result.getResponse().getContentAsString())
+                                .isEqualTo("이미 등록된 질문입니다.");
+                    });
+            verify(questionService, times(1)).post(any(Question.class));
+        }
 
     @Test
     public void 랜덤_질문_조회_성공() throws Exception {
