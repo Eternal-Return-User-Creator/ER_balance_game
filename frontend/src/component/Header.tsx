c0ea8127adcbd436d84f26d850988f4ec4a97d1b
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-26 08:44:15 +0900

    feat(header): html 요소 추가

--- frontend/src/component/Header.tsx
+++ frontend/src/component/Header.tsx
@@ -1,7 +1,16 @@
 export default function Header() {
   return (
     <header>
-      <h1>Header</h1>
+      <h1>ER-BAL</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href={'#'}>처음으로</a>
+            <a href={'#'}>게임시작</a>
+            <a href={'#'}>만들기</a>
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
