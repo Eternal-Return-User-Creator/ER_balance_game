684e85a0356338f40b012c739405cc8c877320cb
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-26 09:01:37 +0900

    feat(font): 폰트 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './assets/css/index.css'
 import Main from './component/Main.tsx'
 import Header from './component/Header.tsx'
 import Footer from './component/Footer.tsx'
