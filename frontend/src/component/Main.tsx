8121e6dcef7b2589fe3aac263e72b1474db6d4aa
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-26 11:30:06 +0900

    feat(main): 메인 페이지 html 요소 추가

--- frontend/src/component/Main.tsx
+++ frontend/src/component/Main.tsx
@@ -1,9 +1,12 @@
 import '../assets/css/Main.css'
+import Banner from '../assets/images/banner.png'
 
 function Main() {
   return (
     <main>
-      <h1>Main</h1>
+      <img src={Banner} alt="배너 이미지" />
+      <h1>이터널 리턴 밸런스 게임</h1>
+      <button>게임 시작</button>
     </main>
   )
 }
