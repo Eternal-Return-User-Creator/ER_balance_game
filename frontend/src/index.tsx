f1068d26985b2eed4b33d6c72cf4b99e546a723c
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-26 09:33:37 +0900

    feat(header): index.tsx에 헤더 css 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/index.css'
+import './assets/css/Header.css'
 import Main from './component/Main.tsx'
 import Header from './component/Header.tsx'
 import Footer from './component/Footer.tsx'
