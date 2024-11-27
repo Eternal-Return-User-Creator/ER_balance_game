9a79f5dc961e0019caadeb476003ea0c812126d2
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-27 10:41:32 +0900

    feat(main): 로고, 타이틀, 버튼에 애니메이션 추가

--- frontend/src/component/Main.tsx
+++ frontend/src/component/Main.tsx
@@ -1,14 +1,23 @@
 import '../assets/css/Main.css'
 import Banner from '../assets/images/banner.png'
+import { useEffect, useState } from "react";
 
 function Main() {
+  const [ isAnimated, setIsAnimated ] = useState(false);
+
+  useEffect(() => {
+    // 컴포넌트가 로드되면 애니메이션 클래스 추가
+    const timer = setTimeout(() => setIsAnimated(true), 0);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <main>
-      <img src={Banner} alt="배너 이미지" />
-      <h1>이터널 리턴 밸런스 게임</h1>
-      <button>게임 시작</button>
+      <img src={ Banner } alt="배너 이미지" className={ `hidden ${ isAnimated ? `animate img-animation` : `` }` }/>
+      <h1 className={ `hidden ${ isAnimated ? `animate title-animation` : `` }` }>이터널 리턴 밸런스 게임</h1>
+      <button className={ `hidden ${ isAnimated ? `animate button-animation` : `` }` }>게임 시작</button>
     </main>
-  )
+  );
 }
 
 export default Main
