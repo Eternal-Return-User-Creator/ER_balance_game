21286ef4faafd3a85b5fe82fbbb1a2dec040dff9
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-28 21:27:33 +0900

    feat(question): 질문 생성 페이지 html 작성

--- frontend/src/page/CreateQuestion.tsx
+++ frontend/src/page/CreateQuestion.tsx
@@ -1,5 +1,34 @@
+import Default from "../assets/images/character/Charlotte/280. Prayge.png"
+
 export default function CreateQuestion() {
   return (
-    <div></div>
+    <div>
+      <h1>질문 만들기</h1>
+      <div className={"wrapper"}>
+        <div className={"helper-wrapper"}>
+          <div className={"speech-bubble"}>
+            질문을 작성하시는 걸 도와드릴게요!
+          </div>
+          <div className={"helper"}>
+            <img src={Default} alt={"캐릭터 기본 이미지"} className={"helper-img"}/>
+          </div>
+        </div>
+        <div className={ "question-wrapper" }>
+          <form>
+            <div className={"input-wrapper"}>
+              <p>질문</p>
+              <input className={"question"} type={ "text" } placeholder={ "질문을 입력해주세요" }/>
+              <p>선택지 A</p>
+              <input className={"choice"} type={ "text" } placeholder={ "선택지를 입력해주세요" }/>
+              <p>선택지 B</p>
+              <input className={"choice"} type={ "text" } placeholder={ "선택지를 입력해주세요" }/>
+            </div>
+            <div>
+              <button type={ "submit" }>만들기</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
