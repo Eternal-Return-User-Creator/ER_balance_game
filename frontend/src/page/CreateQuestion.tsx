4e92e823cdc2992299222241832fee18418c5e6f
yena <50291995+nyj001012@users.noreply.github.com>
2024-11-30 10:43:08 +0900

    feat(question): 입력란 포커스 시, 그에 맞는 설명 출력 기능 추가

--- frontend/src/page/CreateQuestion.tsx
+++ frontend/src/page/CreateQuestion.tsx
@@ -1,8 +1,28 @@
 import Icon from "../assets/images/helper-icon.png"
 import Default from "../assets/images/character/Charlotte/280. Prayge.png"
 import "../assets/css/CreateQuestion.css"
+import { useState } from "react";
 
 export default function CreateQuestion() {
+  const [ description, setDescription ] = useState(<>질문을 작성하시는 걸 도와드릴게요!</>);
+
+  function describeQuestion() {
+    setDescription(<>
+        질문을 입력하는 곳이에요.<br/>
+        "선호하는 것"이나 "더 싫은 것" 등을 물어보세요!<br/>
+        아니면 "어떤 것이 더 좋은 선택일까요?"와 같은 질문도 좋아요.
+      </>
+    );
+  }
+
+  function describeChoice() {
+    setDescription(<>
+        선택지를 입력하는 곳이에요.<br/>
+        질문에 대한 답변을 두 가지로 나눠주세요.
+      </>
+    );
+  }
+
   return (
     <div className={ "content-wrapper" }>
       <div className={ "wrapper" }>
@@ -14,7 +34,7 @@ export default function CreateQuestion() {
             <div className={ "guide-section" }>
               <div className={ "speech-bubble" }>
                 <p className={ "title" }>질문 만들기</p>
-                <p className={ "description" }>질문을 작성하시는 걸 도와드릴게요!</p>
+                <p className={ "description" }>{ description }</p>
               </div>
               <div className={ "helper" }>
                 <img src={ Default } alt={ "캐릭터 기본 이미지" } className={ "helper-img" }/>
@@ -28,13 +48,13 @@ export default function CreateQuestion() {
               <div className={ "input-wrapper" }>
                 <p>질문</p>
                 <input className={ "question" } type={ "text" } placeholder={ "질문을 입력해주세요 (100자 이내)" } tabIndex={ 1 }
-                       maxLength={ 100 } minLength={ 1 }/>
+                       maxLength={ 100 } minLength={ 1 } onFocus={ describeQuestion }/>
                 <p>선택지 A</p>
                 <input className={ "choice" } type={ "text" } placeholder={ "선택지를 입력해주세요 (100자 이내)" } tabIndex={ 2 }
-                       maxLength={ 100 } minLength={ 1 }/>
+                       maxLength={ 100 } minLength={ 1 } onFocus={ describeChoice }/>
                 <p>선택지 B</p>
                 <input className={ "choice" } type={ "text" } placeholder={ "선택지를 입력해주세요 (100자 이내)" } tabIndex={ 3 }
-                       maxLength={ 100 } minLength={ 1 }/>
+                       maxLength={ 100 } minLength={ 1 } onFocus={ describeChoice }/>
               </div>
               <div>
                 <button type={ "submit" } tabIndex={ 4 }>만들기</button>
@@ -44,6 +64,5 @@ export default function CreateQuestion() {
         </div>
       </div>
     </div>
-  )
-    ;
+  );
 }
