9a4537a3a11c0171886d5ac5954cf0fe58bd4cfa
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-03 08:48:13 +0900

    feat(question): 에러 메시지에 입력 예시 추가

--- frontend/src/common/message/charlotteMessage.tsx
+++ frontend/src/common/message/charlotteMessage.tsx
@@ -1,4 +1,6 @@
 export const charlotteErrorMessages = {
-  emptyInputQuestion: <>질문을 적어주셔야 해요.</>,
-  emptyInputChoice: <>선택지를 적어주셔야 해요.</>,
+  emptyInputQuestion: <>질문을 적어주셔야 해요.<br/>
+    예를 들면 "좋아하는 것" 또는 어떤 상황 등이 있어요.</>,
+  emptyInputChoice: <>선택지를 적어주셔야 해요.<br/>
+    질문에 대한 답변을 두 가지로 나눠주세요.</>,
 }
