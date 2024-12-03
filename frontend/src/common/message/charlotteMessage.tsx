79f4032801ac734fe74cc1da3123cea1ae9931a7
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-03 09:01:46 +0900

    feat(question): 입력란이 비워있을 경우, 메시지 출력 및 해당 input focus

--- frontend/src/common/message/charlotteMessage.tsx
+++ frontend/src/common/message/charlotteMessage.tsx
@@ -1,3 +1,12 @@
+export const charlotteMessages = {
+  defaultDescription: <>질문을 작성하시는 걸 도와드릴게요!</>,
+  questionDescription: <>질문을 입력하는 곳이에요.<br/>
+    "선호하는 것"이나 "더 싫은 것" 등을 물어보세요!<br/>
+    아니면 "어떤 것이 더 좋은 선택일까요?"와 같은 질문도 좋아요.</>,
+  choiceDescription: <>선택지를 입력하는 곳이에요.<br/>
+    질문에 대한 답변을 두 가지로 나눠주세요.</>,
+}
+
 export const charlotteErrorMessages = {
   emptyInputQuestion: <>질문을 적어주셔야 해요.<br/>
     예를 들면 "좋아하는 것" 또는 어떤 상황 등이 있어요.</>,
--- frontend/src/page/CreateQuestion.tsx
+++ frontend/src/page/CreateQuestion.tsx
@@ -1,40 +1,46 @@
 import Icon from "../assets/images/helper-icon.png"
 import Default from "../assets/images/character/Charlotte/280. Prayge.png"
+import Error from "../assets/images/character/Charlotte/278. No Way!.png"
 import "../assets/css/CreateQuestion.css"
 import { useState } from "react";
-import { charlotteErrorMessages } from "../common/message/charlotteMessage.ts";
+import { charlotteMessages, charlotteErrorMessages } from "../common/message/charlotteMessage.tsx";
 
 export default function CreateQuestion() {
-  const [ description, setDescription ] = useState(<>질문을 작성하시는 걸 도와드릴게요!</>);
+  const [ description, setDescription ] = useState(charlotteMessages.defaultDescription);
+  const [ image, setImage ] = useState(Default);
   const [ question, setQuestion ] = useState("");
   const [ choiceA, setChoiceA ] = useState("");
   const [ choiceB, setChoiceB ] = useState("");
 
   function describeQuestion() {
-    setDescription(<>
-        질문을 입력하는 곳이에요.<br/>
-        "선호하는 것"이나 "더 싫은 것" 등을 물어보세요!<br/>
-        아니면 "어떤 것이 더 좋은 선택일까요?"와 같은 질문도 좋아요.
-      </>
-    );
+    setDescription(charlotteMessages.questionDescription);
+    setImage(Default);
   }
 
   function describeChoice() {
-    setDescription(<>
-        선택지를 입력하는 곳이에요.<br/>
-        질문에 대한 답변을 두 가지로 나눠주세요.
-      </>
-    );
+    setDescription(charlotteMessages.choiceDescription);
+    setImage(Default);
   }
 
   function validateContent(event: any) {
     event.preventDefault();
     if (question === "") {
+      const questionInput = document.querySelector(".question") as HTMLInputElement;
+      questionInput.focus();
       setDescription(charlotteErrorMessages.emptyInputQuestion);
+      setImage(Error);
       return;
     }
     if (choiceA === "" || choiceB === "") {
+      const choiceInputs = document.querySelectorAll(".choice");
+      if (choiceA === "") {
+        (choiceInputs[0] as HTMLInputElement).focus();
+      }
+      else {
+        (choiceInputs[1] as HTMLInputElement).focus();
+      }
       setDescription(charlotteErrorMessages.emptyInputChoice);
+      setImage(Error);
       return;
     }
   }
@@ -53,7 +59,7 @@ export default function CreateQuestion() {
                 <p className={ "description" }>{ description }</p>
               </div>
               <div className={ "helper" }>
-                <img src={ Default } alt={ "캐릭터 기본 이미지" } className={ "helper-img" }/>
+                <img src={ image } alt={ "캐릭터 기본 이미지" } className={ "helper-img" }/>
                 <hr></hr>
                 <p>Helper. 샬럿</p>
               </div>
