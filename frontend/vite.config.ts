9a660140944e638fef9cc5a898791d5ce19f3622
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-03 11:52:02 +0900

    feat(config): vite에서 develop과 product에 따라 env 다르게 불러오도록 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { localBackendURL } from "./src/common/constants.ts";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [ react() ],
-  server: {
-    proxy: {
-      '/api': {
-        target: localBackendURL,
-        changeOrigin: true,
+
+export default (mode: string) => {
+  // Load app-level env vars to node-level env vars.
+
+  process.env = {
+    ...process.env,
+    ...loadEnv(mode, process.cwd())
+  };
+
+  return defineConfig({
+    plugins: [ react() ],
+    server: {
+      proxy: {
+        '/api': {
+          target: process.env.VITE_BACKEND_URL,
+          changeOrigin: true,
+        }
       }
     }
-  }
-});
+    // To access env vars here use process.env.TEST_VAR
+  });
+}
