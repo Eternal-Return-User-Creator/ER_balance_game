24bdb4167df7521b9f7f32646296af01d7bca711
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-03 09:56:17 +0900

    feat(message): 샬럿 메시지 추가 (200, 욕설)

--- frontend/src/common/message/charlotteMessage.tsx
+++ frontend/src/common/message/charlotteMessage.tsx
@@ -4,6 +4,9 @@ export const charlotteMessages = {
     "선호하는 것"이나 "더 싫은 것" 등을 물어보세요!</>,
   choiceDescription: <>선택지를 입력하는 곳이에요.<br/>
     질문에 대한 답변을 두 가지로 나눠주세요.</>,
+  createDescription: <>질문을 만들고 있어요.<br/>루미아 섬에 잘 전달되었으면...</>,
+  createSuccessDescription: <>질문이 성공적으로 만들어졌어요!<br/>
+    이제 게임을 하러 가거나, 질문을 더 만들어 볼까요?</>
 }
 
 export const charlotteErrorMessages = {
@@ -11,4 +14,6 @@ export const charlotteErrorMessages = {
     예를 들면 "좋아하는 것" 또는 어떤 상황 등이 있어요.</>,
   emptyInputChoice: <>선택지를 적어주셔야 해요.<br/>
     질문에 대한 답변을 두 가지로 나눠주세요.</>,
+  badWordInput: <>욕설이나 무례한 말은 사용하실 수 없어요.<br/>
+    다시 입력해주세요.</>,
 }
