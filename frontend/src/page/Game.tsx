7fa8e8d692d79b381b5b894a948a324fc39f4ab4
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-04 10:48:27 +0900

    feat(game): 버튼에 tab index 지정

--- frontend/src/page/Game.tsx
+++ frontend/src/page/Game.tsx
@@ -2,23 +2,23 @@ import { useState } from "react";
 import "../assets/css/Game.css";
 
 export default function Game() {
-  const [question, setQuestion] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
-  const [choiceA, setChoiceA] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
-  const [choiceB, setChoiceB] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
+  const [ question, setQuestion ] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
+  const [ choiceA, setChoiceA ] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
+  const [ choiceB, setChoiceB ] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
 
   return (
-    <div className={"content-wrapper"}>
-      <div className={"question-wrapper"}>
-        <p className={"question"}>{question}</p>
+    <div className={ "content-wrapper" }>
+      <div className={ "question-wrapper" }>
+        <p className={ "question" }>{ question }</p>
       </div>
-      <div className={"choice-wrapper"}>
+      <div className={ "choice-wrapper" }>
         <form>
-          <button className={"choice"} type={"submit"} name={"choiceA"}>A. {choiceA}</button>
-          <button className={"choice"} type={"submit"} name={"choiceB"}>B. {choiceB}</button>
+          <button className={ "choice" } type={ "submit" } name={ "choiceA" } tabIndex={ 1 }>A. { choiceA }</button>
+          <button className={ "choice" } type={ "submit" } name={ "choiceB" } tabIndex={ 2 }>B. { choiceB }</button>
         </form>
       </div>
       <div className={ "next-button-wrapper" }>
-        <button className={ "next-button" }>&gt;&gt;</button>
+        <button className={ "next-button" } tabIndex={ 3 }>&gt;&gt;</button>
       </div>
     </div>
   )
