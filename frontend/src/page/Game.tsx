c50278b478906af1591eb5f882bdcba848409129
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-04 10:29:27 +0900

    feat(game): question, choiceA, choiceB 추가

--- frontend/src/page/Game.tsx
+++ frontend/src/page/Game.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
+import "../assets/css/Game.css";
+
 export default function Game() {
+  const [question, setQuestion] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
+  const [choiceA, setChoiceA] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
+  const [choiceB, setChoiceB] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque tincidunt justo, non mi.");
+
   return (
     <div className={"content-wrapper"}>
       <div className={"question-wrapper"}>
-        <p className={"question"}></p>
+        <p className={"question"}>{question}</p>
       </div>
       <div className={"choice-wrapper"}>
         <form>
-          <button className={"choice"} type={"submit"} name={"choiceA"}></button>
-          <button className={"choice"} type={"submit"} name={"choiceB"}></button>
+          <button className={"choice"} type={"submit"} name={"choiceA"}>A. {choiceA}</button>
+          <button className={"choice"} type={"submit"} name={"choiceB"}>B. {choiceB}</button>
         </form>
       </div>
       <div className={ "next-button-wrapper" }>
