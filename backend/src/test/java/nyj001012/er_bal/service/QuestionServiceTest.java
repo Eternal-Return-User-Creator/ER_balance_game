612b0649d83e00fe3095202d7a37f09a6d091c83
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-11 13:40:19 +0900

    test(question): 모든 문자열에 대해 빈 문자 검사

--- backend/src/test/java/nyj001012/er_bal/service/QuestionServiceTest.java
+++ backend/src/test/java/nyj001012/er_bal/service/QuestionServiceTest.java
@@ -67,11 +67,26 @@ public class QuestionServiceTest {
             assertThat(e.getMessage()).isEqualTo("질문은 100자 이하이어야 합니다.");
         }
 
+        /**
+         * 비어있는 질문을 제공하는 테스트 메소드
+         * @return 비어있는 질문
+         */
+        private static Stream<Arguments> provideEmptyQuestions() {
+            return Stream.of(
+                    Arguments.of("      ", "선택지 A", "선택지 B"),
+                    Arguments.of("\r\n", "선택지 A", "선택지 B"),
+                    Arguments.of("질문", "", "선택지 B"),
+                    Arguments.of("질문", "\n", "선택지 B"),
+                    Arguments.of("질문", "선택지 A", "\t")
+            );
+        }
+
         @ParameterizedTest
-        @EmptySource
-        @ValueSource(strings = {"", " ", "  ", "\t", "\n", "\r", "\r\n"})
-        public void 질문이_비어있을_경우(String questionText) {
+        @MethodSource("provideEmptyQuestions")
+        public void 질문이_비어있을_경우(String questionText, String choiceA, String choiceB) {
             question.setQuestionText(questionText);
+            question.setChoiceA(choiceA);
+            question.setChoiceB(choiceB);
             IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> questionService.validateQuestionLength(question));
             assertThat(e.getMessage()).isEqualTo("질문은 비어있을 수 없습니다.");
         }
