a34bbc6dc7ba313c67e1cdb598c606535ad54f55
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-20 15:22:24 +0900

    test(question): input 태그 두 개는 각각 테스트 진행

--- frontend/src/test/page/CreateQuestion.test.tsx
+++ frontend/src/test/page/CreateQuestion.test.tsx
@@ -1,14 +1,16 @@
-import { describe, test } from "node:test";
+import { describe, it } from "@jest/globals";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CreateQuestion from "../../page/CreateQuestion.tsx";
 
 describe("CreateQuestion Page", () => {
-  test('컴포넌트 렌더링 확인', () => {
+  it('컴포넌트 렌더링 확인', () => {
     render(<CreateQuestion/>);
     expect(screen.getByText('질문 만들기')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)')).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('선택지를 입력해주세요 (100자 이내)')).toBeInTheDocument();
+    screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)').forEach((element) => {
+      expect(element).toBeInTheDocument();
+    });
     expect(screen.getByText('만들기')).toBeInTheDocument();
     expect(screen.getByText('다시 작성')).toBeInTheDocument();
   });
