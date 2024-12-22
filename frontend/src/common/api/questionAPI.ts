6e59752bff80ce5ec818ae06fc17b2abe3fcaf7e
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-22 11:17:02 +0900

    chore(api): 줄바꿈

--- frontend/src/common/api/questionAPI.ts
+++ frontend/src/common/api/questionAPI.ts
@@ -1,8 +1,8 @@
+const backendURL = import.meta.env.VITE_BACKEND_URL;
+
 /**
  * 질문을 생성하는 API를 호출합니다.
  */
-const backendURL = import.meta.env.VITE_BACKEND_URL;
-
 export async function postCreateQuestionAPI(question: string, choiceA: string, choiceB: string) {
   return await fetch(`${ backendURL }/question`, {
     method: "POST",
