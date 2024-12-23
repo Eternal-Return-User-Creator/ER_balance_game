6eb9cbb7d9a44d5040ad0958dc4eacba7b866981
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-24 07:44:53 +0900

    test(question): 욕설로 인한 질문 생성 실패의 경우 테스트 작성

--- frontend/src/test/page/CreateQuestion.test.tsx
+++ frontend/src/test/page/CreateQuestion.test.tsx
@@ -1,20 +1,28 @@
 import { describe, it, expect, vi } from "vitest";
-import { act, render, screen } from "@testing-library/react";
+import { act, cleanup, fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import CreateQuestion from "../../page/CreateQuestion.tsx";
 import { charlotteErrorMessages, charlotteMessages } from "../../common/message/charlotteMessage.tsx";
 import userEvent from "@testing-library/user-event";
 import { jsxToString } from "../../common/util/format.ts";
 import Success from "../../assets/images/character/Charlotte/277. A Pure Heart.png";
+import Fail from "../../assets/images/character/Charlotte/279. Heartbroken.png";
+import { postCreateQuestionAPI } from "../../common/api/questionAPI.ts";
 
 describe("CreateQuestion Page", () => {
   beforeEach(async () => {
-    await act(async () => {
-      render(<CreateQuestion/>);
-    });
   });
+
+  afterEach(() => {
+    cleanup(); // Testing Library에서 DOM 초기화
+  });
+
+
   describe("렌더링 확인", () => {
-    it('컴포넌트 렌더링 확인', () => {
+    it('컴포넌트 렌더링 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       expect(screen.getByText('질문 만들기')).toBeInTheDocument();
       expect(screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)')).toBeInTheDocument();
       screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)').forEach((element) => {
@@ -24,26 +32,38 @@ describe("CreateQuestion Page", () => {
       expect(screen.getByText('다시 작성')).toBeInTheDocument();
     });
 
-    it('질문 입력란 확인', () => {
+    it('질문 입력란 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
       expect(questionInput).toBeInTheDocument();
       expect(questionInput).toHaveAttribute('maxlength', '100');
     });
 
-    it('선택지 입력란 확인', () => {
+    it('선택지 입력란 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)').forEach((element) => {
         expect(element).toBeInTheDocument();
         expect(element).toHaveAttribute('maxlength', '100');
       });
     });
 
-    it('만들기 버튼 확인', () => {
+    it('만들기 버튼 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const submitButton = screen.getByText('만들기');
       expect(submitButton).toBeInTheDocument();
       expect(submitButton).toBeEnabled();
     });
 
-    it('다시 작성 버튼 확인', () => {
+    it('다시 작성 버튼 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const resetButton = screen.getByText('다시 작성');
       expect(resetButton).toBeInTheDocument();
       expect(resetButton).not.toBeVisible();
@@ -51,19 +71,28 @@ describe("CreateQuestion Page", () => {
   })
 
   describe("포커스 확인", () => {
-    it('질문 입력란 포커스 확인', () => {
+    it('질문 입력란 포커스 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
       expect(questionInput).not.toHaveFocus();
     });
 
-    it('선택지 입력란 포커스 확인', () => {
+    it('선택지 입력란 포커스 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const choiceInputs = screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)');
       choiceInputs.forEach((element) => {
         expect(element).not.toHaveFocus();
       });
     });
 
-    it('질문 입력란 포커스 이동 확인', () => {
+    it('질문 입력란 포커스 이동 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
       act(() => {
         questionInput.focus();
@@ -71,7 +100,10 @@ describe("CreateQuestion Page", () => {
       expect(questionInput).toHaveFocus();
     });
 
-    it('선택지 입력란 포커스 이동 확인', () => {
+    it('선택지 입력란 포커스 이동 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const choiceInputs = screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)');
       act(() => {
         choiceInputs.forEach((element) => {
@@ -84,6 +116,9 @@ describe("CreateQuestion Page", () => {
 
   describe("질문 입력 후 프론트엔드 유효성 검사", () => {
     it('질문 입력란 에러 메시지 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const submitButton = screen.getByText('만들기');
       const description = screen.getByTestId('description');
 
@@ -96,6 +131,9 @@ describe("CreateQuestion Page", () => {
     });
 
     it('선택지 입력란 에러 메시지 확인', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const submitButton = screen.getByText('만들기');
       const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
       const description = screen.getByTestId('description');
@@ -110,6 +148,9 @@ describe("CreateQuestion Page", () => {
     });
 
     it('질문 입력란 에러 메시지 확인 후 포커스 이동', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const submitButton = screen.getByText('만들기');
 
       await act(async () => {
@@ -120,6 +161,9 @@ describe("CreateQuestion Page", () => {
     });
 
     it('선택지 입력란 에러 메시지 확인 후 포커스 이동', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const submitButton = screen.getByText('만들기');
       const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
 
@@ -134,7 +178,10 @@ describe("CreateQuestion Page", () => {
   });
 
   describe("질문 생성 중 기능 테스트", () => {
-    it ("질문 생성 중 설명 텍스트 검사", async () => {
+    it("질문 생성 중 설명 텍스트 검사", async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const submitButton = screen.getByText('만들기');
       const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
       const choiceInputs = screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)');
@@ -154,6 +201,9 @@ describe("CreateQuestion Page", () => {
 
   describe("질문 생성 테스트", () => {
     it('질문 생성 성공', async () => {
+      await act(async () => {
+        render(<CreateQuestion/>);
+      });
       const submitButton = screen.getByText('만들기');
       const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
       const choiceInputs = screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)');
@@ -177,8 +227,30 @@ describe("CreateQuestion Page", () => {
     });
 
     describe("질문 생성 실패", () => {
-      it("욕설 입력", () => {
+      it("욕설 입력", async () => {
+        const mockResponse = {
+          status: 400,
+          text: async () => "욕설은 사용할 수 없습니다.",
+        };
+        vi.stubGlobal('fetch', vi.fn().mockResolvedValueOnce(mockResponse as Response));
 
+        await act(async () => {
+          render(<CreateQuestion/>);
+        });
+        const submitButton = screen.getByText('만들기');
+        const questionInput = screen.getByPlaceholderText('질문을 입력해주세요 (100자 이내)');
+        const choiceInputs = screen.getAllByPlaceholderText('선택지를 입력해주세요 (100자 이내)');
+        const description = screen.getByTestId('description');
+        const image = screen.getByTestId('helper-img');
+
+        await userEvent.type(questionInput, '욕설 ㅅㅂ');
+        await userEvent.type(choiceInputs[0], '선택지 A');
+        await userEvent.type(choiceInputs[1], '선택지 B');
+        await userEvent.click(submitButton);
+
+        const descriptionMessage = jsxToString(charlotteErrorMessages.badWordInput);
+        expect(description.innerHTML).toContain(descriptionMessage);
+        expect(image).toHaveAttribute('src', Fail);
       });
 
       it("같은 선택지 입력", () => {
