e8e201b95d13e12c5b8dc63dd1bf720c7512d463
yena <50291995+nyj001012@users.noreply.github.com>
2024-12-25 10:21:33 +0900

    test(game): 게임 페이지 테스트 구조 작성

--- frontend/src/test/page/Game.test.tsx
+++ frontend/src/test/page/Game.test.tsx
@@ -5,5 +5,23 @@ const mockFetch = (response: Response) => {
 }
 
 describe("Game Page", () => {
+  it("기본 상태 렌더링", async () => {
 
+  });
+
+  it ("선택지 A를 선택했을 때", async () => {
+
+  });
+
+  it ("선택지 B를 선택했을 때", async () => {
+
+  });
+
+  it ("다음 질문을 가져오는 함수를 호출했을 때", async () => {
+
+  });
+
+  it ("백엔드 API 호출 시 에러가 발생했을 때 에러 모달 노출", async () => {
+
+  });
 });
