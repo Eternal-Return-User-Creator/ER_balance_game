1667730a2071451807b37e9fa93a089853bff84e
yena <50291995+nyj001012@users.noreply.github.com>
2025-01-06 08:19:50 +0900

    test(main): 게임 시작 버튼이 표시되는지 테스트

--- e2e-tests/test/main_test.py
+++ e2e-tests/test/main_test.py
@@ -1,6 +1,8 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 @pytest.fixture(scope="module")
@@ -13,3 +15,10 @@ def driver():
 
 def test_타이틀_표시(driver):
     assert driver.title == "이터널리턴 밸런스게임"
+
+def test_게임_시작_버튼_표시(driver):
+    # 게임 시작 버튼의 애니메이션이 끝날 때까지 기다림(최대 10초)
+    game_start_button = WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.ID, "game-start-button"))
+    )
+    assert game_start_button.text == "게임 시작"
