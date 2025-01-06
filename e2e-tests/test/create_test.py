9d8c4ede95af9ba877358483761bd971fd24d93e
yena <50291995+nyj001012@users.noreply.github.com>
2025-01-06 12:18:48 +0900

    test(question): 질문 생성 시의 테스트 케이스 추가

--- e2e-tests/test/create_test.py
+++ e2e-tests/test/create_test.py
@@ -20,23 +20,40 @@ def test_질문_생성_페이지_표시(driver):
     assert create_button.text == '만들기'
     assert reset_button.is_displayed() == False
 
-def test_질문_생성_성공(driver):
+def test_질문_생성(driver):
+    # 질문을 입력한다.
     question_input = driver.find_element(By.CSS_SELECTOR, 'input.question')
     question_input.send_keys('테스트 질문')
     assert question_input.get_attribute('value') == '테스트 질문'
 
+    # 선택지를 입력한다.
     choice_inputs = driver.find_elements(By.CSS_SELECTOR, 'input.choice')
-    choice_inputs[0].send_keys('선택지 1a')
-    choice_inputs[1].send_keys('선택지 2b')
-    assert choice_inputs[0].get_attribute('value') == '선택지 1a'
-    assert choice_inputs[1].get_attribute('value') == '선택지 2b'
+    choice_inputs[0].send_keys('선택지 a')
+    choice_inputs[1].send_keys('선택지 b')
+    assert choice_inputs[0].get_attribute('value') == '선택지 a'
+    assert choice_inputs[1].get_attribute('value') == '선택지 b'
 
+    # 만들기 버튼을 클릭한다.
     create_button = driver.find_element(By.CSS_SELECTOR, 'button.create-question')
     create_button.click()
 
+    # 질문을 생성 중임을 알리는 메시지가 표시된다.
     description = driver.find_element(By.CLASS_NAME, 'description')
     assert description.text == '질문을 만들고 있어요.\n루미아 섬에 잘 전달되었으면...'
 
+    # 질문이 성공적으로 만들어졌음을 알리는 메시지가 표시된다.
     WebDriverWait(driver, 10).until(
         ec.text_to_be_present_in_element((By.CLASS_NAME, 'description'), '질문이 성공적으로 만들어졌어요!\n이제 게임을 하러 가거나, 질문을 더 만들어 볼까요?')
     )
+
+    # 만들기 버튼이 사라지고 초기화 버튼이 표시된다.
+    assert create_button.is_displayed() == False
+    reset_button = driver.find_element(By.CLASS_NAME, 'reset')
+    assert reset_button.is_displayed() == True
+    reset_button.click()
+
+    # 초기화 버튼을 클릭하면 입력한 내용이 초기화되고, 만들기 버튼이 다시 표시된다.
+    assert question_input.get_attribute('value') == ''
+    assert choice_inputs[0].get_attribute('value') == ''
+    assert choice_inputs[1].get_attribute('value') == ''
+    assert reset_button.is_displayed() == False
