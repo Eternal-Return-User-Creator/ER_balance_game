3928962e09b023d2a1516661cb9c83df6612bddb
yena <50291995+nyj001012@users.noreply.github.com>
2025-01-06 12:34:51 +0900

    test(question): 질문 생성 실패했을 경우 테스트 구조 작성

--- e2e-tests/test/create_test.py
+++ e2e-tests/test/create_test.py
@@ -39,31 +39,41 @@ class Test_질문_생성:
             assert choice_inputs[0].get_attribute('value') == '오메가'
             assert choice_inputs[1].get_attribute('value') == '알파'
 
-        def test_질문_생성_버튼_클릭(self, driver):
-            # 만들기 버튼 클릭
-            create_button = driver.find_element(By.CSS_SELECTOR, 'button.create-question')
-            create_button.click()
+        # def test_질문_생성_버튼_클릭(self, driver):
+        #     # 만들기 버튼 클릭
+        #     create_button = driver.find_element(By.CSS_SELECTOR, 'button.create-question')
+        #     create_button.click()
+        #
+        #     # 생성 중 메시지 확인
+        #     description = driver.find_element(By.CLASS_NAME, 'description')
+        #     assert description.text == '질문을 만들고 있어요.\n루미아 섬에 잘 전달되었으면...'
+        #
+        #     WebDriverWait(driver, 10).until(
+        #         ec.text_to_be_present_in_element((
+        #         By.CLASS_NAME, 'description'), '질문이 성공적으로 만들어졌어요!\n이제 게임을 하러 가거나, 질문을 더 만들어 볼까요?')
+        #     )
+        #
+        # def test_초기화_버튼_눌렀을_때_입력_내용_초기화(self, driver):
+        #     # 초기화 버튼 클릭
+        #     reset_button = driver.find_element(By.CLASS_NAME, 'reset')
+        #     assert reset_button.is_displayed()
+        #     reset_button.click()
+        #
+        #     # 입력 초기화 확인
+        #     question_input = driver.find_element(By.CSS_SELECTOR, 'input.question')
+        #     choice_inputs = driver.find_elements(By.CSS_SELECTOR, 'input.choice')
+        #
+        #     assert question_input.get_attribute('value') == ''
+        #     assert choice_inputs[0].get_attribute('value') == ''
+        #     assert choice_inputs[1].get_attribute('value') == ''
+        #     assert not reset_button.is_displayed()
 
-            # 생성 중 메시지 확인
-            description = driver.find_element(By.CLASS_NAME, 'description')
-            assert description.text == '질문을 만들고 있어요.\n루미아 섬에 잘 전달되었으면...'
+    class Test_질문_생성_실패:
+        def test_질문에_욕설_포함(self, driver):
+            pass
 
-            WebDriverWait(driver, 10).until(
-                ec.text_to_be_present_in_element((
-                By.CLASS_NAME, 'description'), '질문이 성공적으로 만들어졌어요!\n이제 게임을 하러 가거나, 질문을 더 만들어 볼까요?')
-            )
+        def test_선택지_중복(self, driver):
+            pass
 
-        def test_초기화_버튼(self, driver):
-            # 초기화 버튼 클릭
-            reset_button = driver.find_element(By.CLASS_NAME, 'reset')
-            assert reset_button.is_displayed()
-            reset_button.click()
-
-            # 입력 초기화 확인
-            question_input = driver.find_element(By.CSS_SELECTOR, 'input.question')
-            choice_inputs = driver.find_elements(By.CSS_SELECTOR, 'input.choice')
-
-            assert question_input.get_attribute('value') == ''
-            assert choice_inputs[0].get_attribute('value') == ''
-            assert choice_inputs[1].get_attribute('value') == ''
-            assert not reset_button.is_displayed()
+        def test_질문_중복(self, driver):
+            pass
