608307e93744c293c14c60cbaa41639235dac253
yena <50291995+nyj001012@users.noreply.github.com>
2025-01-06 12:32:08 +0900

    test(question): 질문 생성 성공 테스트 케이스 추가

--- e2e-tests/test/create_test.py
+++ e2e-tests/test/create_test.py
@@ -9,6 +9,7 @@ def driver(driver):
     driver.get('http://localhost:3000/create')
     yield driver
 
+
 def test_질문_생성_페이지_표시(driver):
     title = driver.find_element(By.CLASS_NAME, 'title')
     description = driver.find_element(By.CLASS_NAME, 'description')
@@ -18,42 +19,51 @@ def test_질문_생성_페이지_표시(driver):
     assert title.text == '질문 만들기'
     assert description.text == '질문을 작성하시는 걸 도와드릴게요!'
     assert create_button.text == '만들기'
-    assert reset_button.is_displayed() == False
+    assert not reset_button.is_displayed()
 
-def test_질문_생성(driver):
-    # 질문을 입력한다.
-    question_input = driver.find_element(By.CSS_SELECTOR, 'input.question')
-    question_input.send_keys('테스트 질문')
-    assert question_input.get_attribute('value') == '테스트 질문'
 
-    # 선택지를 입력한다.
-    choice_inputs = driver.find_elements(By.CSS_SELECTOR, 'input.choice')
-    choice_inputs[0].send_keys('선택지 a')
-    choice_inputs[1].send_keys('선택지 b')
-    assert choice_inputs[0].get_attribute('value') == '선택지 a'
-    assert choice_inputs[1].get_attribute('value') == '선택지 b'
+@pytest.mark.usefixtures('driver')  # driver fixture를 사용하도록 명시
+class Test_질문_생성:
+    class Test_질문_생성_성공:
+        def test_질문_입력(self, driver):
+            # 질문 입력
+            question_input = driver.find_element(By.CSS_SELECTOR, 'input.question')
+            question_input.send_keys('테스트 질문')
+            assert question_input.get_attribute('value') == '테스트 질문'
 
-    # 만들기 버튼을 클릭한다.
-    create_button = driver.find_element(By.CSS_SELECTOR, 'button.create-question')
-    create_button.click()
+        def test_선택지_입력(self, driver):
+            # 선택지 입력
+            choice_inputs = driver.find_elements(By.CSS_SELECTOR, 'input.choice')
+            choice_inputs[0].send_keys('오메가')
+            choice_inputs[1].send_keys('알파')
+            assert choice_inputs[0].get_attribute('value') == '오메가'
+            assert choice_inputs[1].get_attribute('value') == '알파'
 
-    # 질문을 생성 중임을 알리는 메시지가 표시된다.
-    description = driver.find_element(By.CLASS_NAME, 'description')
-    assert description.text == '질문을 만들고 있어요.\n루미아 섬에 잘 전달되었으면...'
+        def test_질문_생성_버튼_클릭(self, driver):
+            # 만들기 버튼 클릭
+            create_button = driver.find_element(By.CSS_SELECTOR, 'button.create-question')
+            create_button.click()
 
-    # 질문이 성공적으로 만들어졌음을 알리는 메시지가 표시된다.
-    WebDriverWait(driver, 10).until(
-        ec.text_to_be_present_in_element((By.CLASS_NAME, 'description'), '질문이 성공적으로 만들어졌어요!\n이제 게임을 하러 가거나, 질문을 더 만들어 볼까요?')
-    )
+            # 생성 중 메시지 확인
+            description = driver.find_element(By.CLASS_NAME, 'description')
+            assert description.text == '질문을 만들고 있어요.\n루미아 섬에 잘 전달되었으면...'
 
-    # 만들기 버튼이 사라지고 초기화 버튼이 표시된다.
-    assert create_button.is_displayed() == False
-    reset_button = driver.find_element(By.CLASS_NAME, 'reset')
-    assert reset_button.is_displayed() == True
-    reset_button.click()
+            WebDriverWait(driver, 10).until(
+                ec.text_to_be_present_in_element((
+                By.CLASS_NAME, 'description'), '질문이 성공적으로 만들어졌어요!\n이제 게임을 하러 가거나, 질문을 더 만들어 볼까요?')
+            )
 
-    # 초기화 버튼을 클릭하면 입력한 내용이 초기화되고, 만들기 버튼이 다시 표시된다.
-    assert question_input.get_attribute('value') == ''
-    assert choice_inputs[0].get_attribute('value') == ''
-    assert choice_inputs[1].get_attribute('value') == ''
-    assert reset_button.is_displayed() == False
+        def test_초기화_버튼(self, driver):
+            # 초기화 버튼 클릭
+            reset_button = driver.find_element(By.CLASS_NAME, 'reset')
+            assert reset_button.is_displayed()
+            reset_button.click()
+
+            # 입력 초기화 확인
+            question_input = driver.find_element(By.CSS_SELECTOR, 'input.question')
+            choice_inputs = driver.find_elements(By.CSS_SELECTOR, 'input.choice')
+
+            assert question_input.get_attribute('value') == ''
+            assert choice_inputs[0].get_attribute('value') == ''
+            assert choice_inputs[1].get_attribute('value') == ''
+            assert not reset_button.is_displayed()
