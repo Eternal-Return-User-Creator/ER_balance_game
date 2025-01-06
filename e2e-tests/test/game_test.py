68a55ae2e4799a9c6dfcad59aa6b3baf9d50e274
yena <50291995+nyj001012@users.noreply.github.com>
2025-01-06 14:25:57 +0900

    test(game): 다음 질문 버튼 클릭 시 선택 상태 초기화 테스트

--- e2e-tests/test/game_test.py
+++ e2e-tests/test/game_test.py
@@ -4,6 +4,15 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 
 
+def check_initial_texts(question, choices, ratios):
+    assert question.text != ''
+    assert len(choices) == 2
+    assert choices[0].text != ''
+    assert choices[1].text != ''
+    assert len(ratios) == 2
+    assert ratios[0].text == ''
+    assert ratios[1].text == ''
+
 @pytest.fixture(scope="module")
 def driver(driver):
     driver.get("http://localhost:3000/game")
@@ -18,13 +27,7 @@ def test_게임_페이지_표시(driver):
     ratios = driver.find_elements(By.CLASS_NAME, 'choice-ratio')
     next_button = driver.find_element(By.CLASS_NAME, 'next-button')
 
-    assert question.text != ''
-    assert len(choices) == 2
-    assert choices[0].text != ''
-    assert choices[1].text != ''
-    assert len(ratios) == 2
-    assert ratios[0].text == ''
-    assert ratios[1].text == ''
+    check_initial_texts(question, choices, ratios)
     assert next_button.is_displayed()
 
 @pytest.mark.parametrize('choice_index', [0, 1])
@@ -43,4 +46,17 @@ def test_선택지_클릭_시_결과_표시(driver, choice_index):
     assert ratios[1].text != ''
 
 def test_다음_질문_버튼_클릭_시_다음_질문_표시(driver):
-    pass
+    driver.get('http://localhost:3000/game')
+
+    next_button = driver.find_element(By.CLASS_NAME, 'next-button')
+    next_button.click()
+
+    WebDriverWait(driver, 10).until(
+        ec.visibility_of_element_located((By.CSS_SELECTOR, 'p.in-game.question'))
+    )
+
+    question = driver.find_element(By.CSS_SELECTOR, 'p.in-game.question')
+    choices = driver.find_elements(By.CLASS_NAME, 'choice-text')
+    ratios = driver.find_elements(By.CLASS_NAME, 'choice-ratio')
+
+    check_initial_texts(question, choices, ratios)
