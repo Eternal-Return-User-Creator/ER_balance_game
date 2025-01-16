197b2b53c193b98180f6ceb2e5ad9de3de86284b
yena <50291995+nyj001012@users.noreply.github.com>
2025-01-16 09:18:04 +0900

    test: headless로 변경

--- e2e-tests/test/conftest.py
+++ e2e-tests/test/conftest.py
@@ -1,10 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chromeOptions
+from selenium.webdriver.firefox.options import Options as firefoxOptions
+from selenium.webdriver.safari.options import Options as safariOptions
 import pytest
 from . import driver_factory
 
 @pytest.fixture(scope="module")
 def driver(request):
     browser = request.config.getoption("browser")
-    driver = driver_factory(browser)
+    if browser == "chrome":
+        webdriver.Chrome()
+        option = chromeOptions()
+        option.add_argument("--headless")
+        driver = webdriver.Chrome(options=option)
+    elif browser == "firefox":
+        webdriver.Firefox()
+        option = firefoxOptions()
+        option.add_argument("--headless")
+        driver = webdriver.Firefox(options=option)
+    elif browser == "safari":
+        webdriver.Safari()
+        option = safariOptions()
+        option.add_argument("--headless")
+        driver = webdriver.Safari(options=option)
+    else:
+        raise ValueError(f"Unsupported browser: {browser}")
     yield driver
     driver.quit()
 
